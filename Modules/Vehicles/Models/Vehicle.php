11ce48b1dd13ae2c18b37736f7c75b38661ebe2e
Iker Vericat <ikervericat@iesmontsia.org>
2026-02-02 18:47:27 +0100

    Fix: Vehicle Model

--- Modules/Vehicles/Models/Vehicle.php
+++ Modules/Vehicles/Models/Vehicle.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace App\Models;
+namespace Modules\Vehicles\Models;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Vehicles\Models\VehicleType;
 
 class Vehicle extends Model
 {
     use HasFactory;
-    protected $fillable = ['license', 'status', 'vehicle_type'];
+    protected $table = 'vehicles';
+    protected $fillable = ['license', 'status', 'vehicle_type_id',];
+
+    public function vehicleType()
+    {
+        return $this->belongsTo(VehicleType::class, 'vehicle_type_id');
+    }
 }
