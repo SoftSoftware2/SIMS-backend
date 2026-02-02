48ded358a99ad51a2b7fc58f4cf31fa1e76d63bc
FrancescQuintoAnguera <francescquinto@iesmontsia.org>
2026-02-02 19:56:09 +0100

    Fixed: Rcuested changes

--- docker/create_dbs.sh
+++ docker/create_dbs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-# Aquí definimos las bases de datos extra que queremos
-# Puedes cambiar 'db_testing' por el nombre que quieras
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE company_1;
-    GRANT ALL PRIVILEGES ON DATABASE company_1 TO "$POSTGRES_USER";
+    CREATE DATABASE "$DB_NEW_DATABASE;
+    GRANT ALL PRIVILEGES ON DATABASE "$DB_NEW_DATABASE" TO "$POSTGRES_USER";
 EOSQL
