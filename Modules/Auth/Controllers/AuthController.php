603f636c8b02e854487f3cafc056871b75a72541
jordiarnau05 <jordiarnau@iesmontsia.org>
2026-02-03 18:46:27 +0100

    Update validation rules for user registration:

--- Modules/Auth/Controllers/AuthController.php
+++ Modules/Auth/Controllers/AuthController.php
@@ -15,8 +15,8 @@ class AuthController extends Controller
     public function register(Request $request): JsonResponse
     {
         $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required|string|max:50',
+            'email' => 'required|string|email|max:70|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
 
